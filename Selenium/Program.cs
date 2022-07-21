00e29868eec36debb8eb6e17e338a9d143b52d48
Celia Gao <celiagao830@gmail.com>
2022-07-21 15:36:00 -0500

    Selenium

---
+++ Selenium/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http:www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Automation Testing");
+            driver.Quit();
+        }
+    }
+}
